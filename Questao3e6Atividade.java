2bab85bad924f0f40918f60e19eb2cefaa3d42e1
Morgan-Sa <71098129+Morgan-Sa@users.noreply.github.com>
2022-05-04 07:55:26 -0300

    Add Questoes 3 e 6

---
+++ Questao3e6Atividade.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Terminei a primeira aula com um programa java!"); // Questao 3
+    System.out.println("Mateus e Ramon programando sem café!"); 
+    System.out.println("Flamengo"); // Questao 6
+  }
+}
