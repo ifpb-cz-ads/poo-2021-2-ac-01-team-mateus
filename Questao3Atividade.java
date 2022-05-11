16e5f2f6699fc9340d37d82f877d7338dc1526bf
raszz <50380670+raszz@users.noreply.github.com>
2022-05-10 23:39:19 -0300

    Alterando titulo.

---
+++ Questao3Atividade.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Terminei a primeira aula com um programa java!"); // Questao 3
+    
+  }
+}
+
+/* alteração feita após rever comentário e sugestão do aluno Bruno. */
+/*class Main {
+  public static void main(String[] args) {
+    System.out.println("Terminei a primeira aula com um programa java!"); // Questao 3
+    System.out.println("Mateus e Ramon programando sem café!"); 
+    System.out.println("Flamengo"); // Questao 6
+  }
+}
+/*
--- Questao3e6Atividade.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Terminei a primeira aula com um programa java!"); // Questao 3
-    
-  }
-}
-
-/* alteração feita após rever comentário do aluno Bruno. */
