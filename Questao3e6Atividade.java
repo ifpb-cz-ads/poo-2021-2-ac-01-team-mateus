617261860f6cde154241ec88a9e76e9ad0ceb397
raszz <50380670+raszz@users.noreply.github.com>
2022-05-10 23:37:43 -0300

    Atualizacao Questao3

--- Questao3e6Atividade.java
+++ Questao3e6Atividade.java
@@ -1,7 +1,8 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Terminei a primeira aula com um programa java!"); // Questao 3
-    System.out.println("Mateus e Ramon programando sem café!"); 
-    System.out.println("Flamengo"); // Questao 6
+    
   }
 }
+
+/* alteração feita após rever comentário do aluno Bruno. */
